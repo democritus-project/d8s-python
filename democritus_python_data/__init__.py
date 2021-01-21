6c2345f686609fe99a1ab970a6d8dff6bb7b1553
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-21 09:19:53 -0500

    Improving version management
    See https://github.com/democritus-project/roadmap/issues/4

--- democritus_python_data/__init__.py
+++ democritus_python_data/__init__.py
@@ -1,5 +1,15 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version('democritus_python_data')
+except PackageNotFoundError:
+    message = 'Unable to find a version number for "democritus_python_data". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-python-data/issues.'
+    print(message)
+
 __author__ = '''Floyd Hightower'''
 __email__ = 'floyd.hightower27@gmail.com'
-__version__ = '2021.01.12'
 
 from .ast_data import *
--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '2021.01.12'
-
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
@@ -15,7 +13,6 @@ with open('requirements_dev.txt') as requirements_dev_file:
 
 setup(
     name='democritus_ast',
-    version=__version__,
     description="Democritus functions for working with Python ASTs.",
     long_description=readme,
     long_description_content_type="text/markdown",
