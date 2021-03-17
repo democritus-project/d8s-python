f657e7bd6bc0a044abab17457777312e36dfdd0e
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-17 08:05:07 -0500

    Adding support for bumpversion
        Fixes https://github.com/democritus-project/roadmap/issues/69

--- d8s_python/__init__.py
+++ d8s_python/__init__.py
@@ -1,14 +1,4 @@
-try:
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:
-    from importlib_metadata import PackageNotFoundError, version  # type: ignore
-
-try:
-    __version__ = version('d8s_python')
-except PackageNotFoundError:
-    message = 'Unable to find a version number for "d8s_python". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-python/issues.'
-    print(message)
-
+__version__ = '0.2.0'
 __author__ = '''Floyd Hightower'''
 __email__ = 'floyd.hightower27@gmail.com'
 
